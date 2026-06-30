d73b76ce1122b25831a991d042deda7a2bc38b04
Ansh Gautam <anshgautam1011@gmail.com>
2026-06-30 13:42:26 +0530

    docs loaded

---
+++ 04- Hybrid Search Strategies/01- Combining Sparse & Dense Retriever.py
@@ -0,0 +1,9 @@
+from langchain.schema import Document
+
+docs = [
+    Document(page_content="LangChain helps build LLM applications."),
+    Document(page_content="Pinecone is a vector database for semantic search."),
+    Document(page_content="The Eiffel Tower is located in Paris."),
+    Document(page_content="LangChain can be used to develop agentic ai applications."),
+    Document(page_content="lancha has many types of retrievers."),
+]
