1de2ea43ef1ce08dfc995e1a406d07fad6ae91b3
Ansh Gautam <anshgautam1011@gmail.com>
2026-06-30 13:49:07 +0530

    dense_vectorstore added

--- 04- Hybrid Search Strategies/01- Combining Sparse & Dense Retriever.py
+++ 04- Hybrid Search Strategies/01- Combining Sparse & Dense Retriever.py
@@ -1,5 +1,8 @@
+from langchain_community.retrievers import BM25Retriever
+from langchain.retrievers import EnsembleRetriever
 from langchain.schema import Document
 
+# Sample docs
 docs = [
     Document(page_content="LangChain helps build LLM applications."),
     Document(page_content="Pinecone is a vector database for semantic search."),
@@ -7,3 +10,8 @@ docs = [
     Document(page_content="LangChain can be used to develop agentic ai applications."),
     Document(page_content="lancha has many types of retrievers."),
 ]
+
+#Dense Retriever
+embedding_model = HuggingFaceEmbeddings(model_name="all-MINI-L6-v2")
+dense_vectorstore = FAISS.from_documents(docs,embedding_model)
+dense_retriever = dense_vectorstore.as_retriever()
